1163c36cdafa584aa54eec26d3e1d966d0377513
swarnim <swarnimjain004@gmail.com>
2023-12-07 12:17:43 +0530

    completed DPLL

--- solver.cpp
+++ solver.cpp
@@ -24,12 +24,9 @@ enum SAT
 
 class SATSolverDPLL
 {
-public:
+private:
     int unitPropogation(CNF &formula, Assignment &assignment);
-    int pureElimination(CNF &formula, Assignment &assignment);
     int chooseLiteral(CNF &formula);
-    bool isFormulaSatisfied(const CNF &formula, const Assignment &assignment);
-    // int chooseLiteral_Random(const CNF &formula, const Assignment &assignment);
 
 public:
     bool solve(CNF &formula);
@@ -37,6 +34,11 @@ public:
 
 int SATSolverDPLL::unitPropogation(CNF &formula, Assignment &assignment)
 {
+    if (formula.size() == 0)
+    {
+        return SAT::satisfied;
+    }
+
     bool unit_clause_found = true;
     while (unit_clause_found)
     {
@@ -92,100 +94,28 @@ int SATSolverDPLL::unitPropogation(CNF &formula, Assignment &assignment)
     return SAT::normal;
 }
 
-int SATSolverDPLL::pureElimination(CNF &formula, Assignment &assignment)
-{
-    std::unordered_set<int> literals;
-    for (Clause &clause : formula)
-    {
-        for (int literal : clause)
-        {
-            literals.insert(literal);
-        }
-    }
-    for (int literal : literals)
-    {
-        if (literals.find(-literal) == literals.end())
-        {
-            assignment.insert(literal);
-            for (auto it = formula.begin(); it != formula.end();)
-            {
-                Clause clause = *it;
-                if (find(clause.begin(), clause.end(), literal) != clause.end())
-                {
-                    it = formula.erase(it);
-                }
-                else
-                {
-                    it++;
-                }
-            }
-        }
-        if (formula.size() == 0)
-        {
-            return SAT::satisfied;
-        }
-    }
-    return SAT::normal;
-}
-
-bool SATSolverDPLL::isFormulaSatisfied(const CNF &formula, const Assignment &assignment)
-{
-    for (const Clause &clause : formula)
-    {
-        bool check = false;
-        for (int literal : clause)
-        {
-            if (assignment.find(literal) != assignment.end())
-            {
-                check = true;
-                break;
-            }
-        }
-
-        if (!check)
-        {
-            return false;
-        }
-    }
-    return true;
-}
-
 int SATSolverDPLL::chooseLiteral(CNF &formula)
 {
     // default strategy
     return *(formula[0].begin());
 }
 
-void SATSolverDPLL::backtrack(CNF &formula, Assignment &assignment, int decisionLevel)
-{
-    // Remove assigned literals since the decision level
-    for (auto it = assignment.rbegin(); it != assignment.rend() && it->first >= decisionLevel; ++it)
-    {
-        assignment.erase(*it);
-    }
-
-    // Unassign the latest assigned literal at the decision level
-    assignment.erase(decisionLevel);
-
-    // Update clauses based on the unassigned literal
-    for (auto it = formula.begin(); it != formula.end(); ++it)
-    {
-        for (auto itt = it->begin(); itt != it->end(); ++itt)
-        {
-            if (*itt == decisionLevel)
-            {
-                // If the literal is found in a clause, erase it
-                it->erase(itt);
-                break;
-            }
-        }
-    }
-}
-
 bool SATSolverDPLL::solve(CNF &formula)
 {
     Assignment assignment = {};
     int decision_level = 0;
+
+    int result = unitPropogation(formula, assignment);
+
+    if (result == SAT::satisfied)
+    {
+        return true;
+    }
+    else if (result == SAT::unsatisfied)
+    {
+        return false;
+    }
+
     while (true)
     {
         int literal = SATSolverDPLL::chooseLiteral(formula);
@@ -204,21 +134,21 @@ bool SATSolverDPLL::solve(CNF &formula)
             }
             else if (result == SAT::unsatisfied)
             {
+                decision_level--;
+                assignment.erase(literal);
+                assignment.insert(-literal);
                 if (decision_level == 0)
                 {
                     return false;
                 }
                 else
                 {
-                    decision_level--;
-                    assignment.erase(literal);
-                    formula = formula_original;
-                    break;
+                    result = unitPropogation(formula, assignment);
                 }
             }
             else
             {
-                result = pureElimination(formula, assignment);
+                break;
             }
         }
     }
@@ -227,18 +157,16 @@ bool SATSolverDPLL::solve(CNF &formula)
 int main()
 {
     // CNF formula = {{-1}, {-3, 4}, {2, 3}, {1, -2}};
-    CNF formula = {{3}, {-1, -5}, {5, 3}, {-3, 4}};
+    CNF formula = {{1}, {-1, -5}, {5, 3}, {4}, {-4}};
     CNF formula_original(formula);
     Assignment assignment = {};
 
     SATSolverDPLL solver;
 
-    solver.unitPropogation(formula, assignment);
-
-    if (/*solver.isFormulaSatisfied(formula, assignment)*/ true)
+    if (solver.solve(formula))
     {
-        std::cout << "SAT:\n";
-        for (auto literal : assignment)
+        std::cout << "SAT\n";
+        /*for (auto literal : assignment)
         {
             std::cout << "Variable " << abs(literal) << ": " << (literal > 0 ? "true" : "false");
             std::cout << "\n";
@@ -251,7 +179,7 @@ int main()
                 std::cout << literal << " ";
             }
             std::cout << "\n";
-        }
+        }*/
     }
     else
     {
