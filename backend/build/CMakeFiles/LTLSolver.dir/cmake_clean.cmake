3d67deb9da4e4e2a328844cc0bed65ed4979d51c
swarnim <swarnimjain004@gmail.com>
2023-12-25 11:55:21 +0530

    configured cmake project for backend

---
+++ backend/build/CMakeFiles/LTLSolver.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LTLSolver.dir/src/main.cpp.o"
+  "CMakeFiles/LTLSolver.dir/src/main.cpp.o.d"
+  "LTLSolver"
+  "LTLSolver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LTLSolver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
